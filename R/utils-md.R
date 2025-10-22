912cd41075e705f6ce50ed580462a9c42bc4cd94
Anna Frances-Abellan <anna.frances@uab.cat>
2025-10-22 08:10:02 +0000

    version 3.7.1

--- R/utils-md.R
+++ R/utils-md.R
@@ -4,12 +4,15 @@
 #'     point to inside the current package.
 #'
 include_help <- function(...) {
+  help_path <- system.file("app/help", ..., package = "biodosetools")
+  tmp_path <- file.path(tempdir(), basename(help_path))
+  file.copy(help_path, tmp_path, overwrite = TRUE)
+
   withMathJax(
-    includeMarkdown(
-      system.file("app/help", ..., package = "biodosetools")
-    )
+    includeMarkdown(tmp_path)
   )
 }
+
 
 #' Load RMarkdown report
 #'
